6609557dd3d85a568d9224b1b327260b5050e891
deancrossett <crossett-d1@ulster.ac.uk>
2021-05-10 21:44:08 +0100

    setting up structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Header from "../src/components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>office roulette</h1>
+      <Header />
       <p>find the best episodes</p>
       <p>test</p>
     </div>
